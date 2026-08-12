258446067425eba10f2218f0c58a19d9adc38eb0
Guy Halse <guy@tenet.ac.za>
2026-08-12 16:29:31 +0200

    Don't write to stdout during unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,7 @@ require_once($projectRoot . '/vendor/autoload.php');
 // Symlink module into ssp vendor lib so that templates and urls can resolve correctly
 $linkPath = $projectRoot . '/vendor/simplesamlphp/simplesamlphp/modules/sqlattribs';
 if (file_exists($linkPath) === false) {
-    echo "Linking '$linkPath' to '$projectRoot'\n";
-    symlink($projectRoot, $linkPath);
+    if (symlink($projectRoot, $linkPath) === false) {
+        throw new RuntimeException("Unable to create test symlink '$linkPath' to '$projectRoot'.");
+    }
 }
